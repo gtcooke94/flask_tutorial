f1f2b3f5ff661b73458f6a550e8c64a6bbb89382
Greg Cooke <gtcooke94@gmail.com>
2019-08-06 16:09:20 -0400

    Finished 6 - Profile page and avatars

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User, Post
 
 
@@ -70,3 +71,43 @@ def register():
         return redirect(url_for("login"))
     return render_template("register.html", title="Register", form=form)
 
+
+# This <username> syntax means that flask will accept anything here, and map
+# whatever is there to the username arguemnt in the user function below
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {"author": user, "body": "Test post 1"},
+        {"author": user, "body": "Test post 2"},
+    ]
+    return render_template("user.html", user=user, posts=posts)
+
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved")
+        return redirect(url_for("edit_profile"))
+    # Prepopulate fields with data in database if it is a GET request
+    # BUT, the above if statement could also fail if there is invalid form
+    # data. In that case, we don't want to prefill and instead just return the
+    # form data and it will render the errors because of how we've written it
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", title="Edit Profile", form=form)
+
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
