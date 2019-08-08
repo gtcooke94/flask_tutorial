9784a9d4c74635a0035a5fcb0e695cd52731b83a
Greg Cooke <gtcooke94@gmail.com>
2019-08-08 10:53:42 -0400

    Through first half of Part 7 - Error Handling.

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,14 @@
 export FLASK_APP=microblog.py
-export FLASK_DEBUG=1
+#export FLASK_DEBUG=1
+export MAIL_SERVER=localhost
+export MAIL_PORT=8025
+
+
+
+# This is how to setup emailing via gmail with the error logging. May have to
+# fiddle with account settings to allow less secure apps to access email
+# export MAIL_SERVER=smtp.googlemail.com
+# export MAIL_PORT=587
+# export MAIL_USE_TLS=1
+# export MAIL_USERNAME=<your-gmail-username>
+# export MAIL_PASSWORD=<your-gmail-password>
--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import SMTPHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -15,4 +17,27 @@ login = LoginManager(app)
 # @login_required decorated imported from flask_login
 login.login_view = "login"
 
-from app import routes, models
+from app import routes, models, errors
+
+# python -m smtpd -n -c DebuggingServer localhost:8025
+# Above command will run a fake email server that accepts emails, but instead
+# of sending them will print them to the console
+# See .flask_env for how we would setup a gmail account email server
+if not app.debug:
+    if app.config["MAIL_SERVER"]:
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr="no-reply@" + app.config["MAIL_SERVER"],
+            toaddrs=app.config["ADMINS"],
+            subject="Microblog Failure",
+            credentials=auth,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
